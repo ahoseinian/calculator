80160475ccaa9fd3b7f1eabc5a3fb1b795f236f2
Amir Hoseinian <amir.hoseinian@aboutyou.de>
2018-12-09 17:31:47 +0100

    Fix numbers

--- src/app.js
+++ src/app.js
@@ -1,106 +1,132 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
+import React from "react";
+import ReactDOM from "react-dom";
 
 class Calculator extends React.Component {
-    
-    state = {
-        results: [],
-        disabled:  true,
-    }
+  state = {
+    results: [],
+    disabled: true
+  };
 
-    handleNumbers = (Value) => {
-        this.setState((prevState) => ({
-            results: prevState.results.concat(Value),
-            disabled: false
-        }))
-    }
+  handleNumbers = Value => {
+    this.setState(prevState => ({
+      results: prevState.results.concat(Value),
+      disabled: false
+    }));
+  };
 
-    handleResult = () => {
-        const result = eval(this.state.results.join('').toString());
-        this.setState({
-            results: [result]
-        })
-    }
+  handleResult = () => {
+    const result = eval(this.state.results.join("").toString());
+    this.setState({
+      results: [result]
+    });
+  };
 
-    handleDisabled = (Value) => {
-        this.setState((prevState) => ({
-            results: prevState.results.concat(Value),
-            disabled: true
-        }))
-    }
+  handleDisabled = Value => {
+    this.setState(prevState => ({
+      results: prevState.results.concat(Value),
+      disabled: true
+    }));
+  };
 
-    handleClear = () => {
-        this.setState({
-            results: []
-        })
-    }
+  handleClear = () => {
+    this.setState({
+      results: []
+    });
+  };
 
-    
-    render(){
-        return(
-            <div>
-                <Buttons 
-                    results={this.state.results} 
-                    disabled={this.state.disabled}
-                    handleDisabled={this.handleDisabled} 
-                    handleNumbers={this.handleNumbers} 
-                    handleResult={this.handleResult}  
-                    handleClear={this.handleClear}
-                />
+  render() {
+    return (
+      <div>
+        <Buttons
+          results={this.state.results}
+          disabled={this.state.disabled}
+          handleDisabled={this.handleDisabled}
+          handleNumbers={this.handleNumbers}
+          handleResult={this.handleResult}
+          handleClear={this.handleClear}
+        />
 
-                <Numbers results={this.state.results} />
-            </div>
-        )
-    }
+        <Numbers results={this.state.results} />
+      </div>
+    );
+  }
 }
 
+function Buttons(props) {
+  const handleClick = e => {
+    const Value = e.target.value;
+    props.handleNumbers(Value);
+  };
 
-class Buttons extends React.Component {
+  const handleDisabled = e => {
+    const Value = e.target.value;
+    props.handleDisabled(Value);
+  };
 
-    handleClick = (e) => {
-        const Value = e.target.value;
-        this.props.handleNumbers(Value);
-    }
-
-    handleDisabled = (e) => {
-        const Value = e.target.value;
-        this.props.handleDisabled(Value);
-
-    }
-
-
-    render(){
-        return(
-            <div>
-                <button onClick={this.handleClick} value="0">0</button>
-                <button onClick={this.handleClick} value="1">1</button>
-                <button onClick={this.handleClick} value="2">2</button>                
-                <button onClick={this.handleClick} value="3">3</button>                
-                <button onClick={this.handleClick} value="4">4</button>                
-                <button onClick={this.handleClick} value="5">5</button>                
-                <button onClick={this.handleClick} value="6">6</button>                
-                <button onClick={this.handleClick} value="7">7</button>                
-                <button onClick={this.handleClick} value="8">8</button>                
-                <button onClick={this.handleClick} value="9">9</button>                               
-                <button onClick={this.handleClick} value=".">.</button>                               
-                <button onClick={this.handleDisabled} disabled={this.props.disabled} value="+">+</button>
-                <button onClick={this.handleDisabled} disabled={this.props.disabled} value="-">-</button>
-                <button onClick={this.handleDisabled} disabled={this.props.disabled} value="*">*</button>
-                <button onClick={this.handleDisabled} disabled={this.props.disabled} value="/">/</button>
-                <button onClick={this.handleDisabled} disabled={this.props.disabled} value="%">%</button>
-                <button onClick={this.props.handleResult} disabled={this.props.disabled}>=</button>
-                <button onClick={this.props.handleClear}>AC</button>
-            </div>
-        )
-    }
+  return (
+    <div>
+      <button onClick={handleClick} value="0">
+        0
+      </button>
+      <button onClick={handleClick} value="1">
+        1
+      </button>
+      <button onClick={handleClick} value="2">
+        2
+      </button>
+      <button onClick={handleClick} value="3">
+        3
+      </button>
+      <button onClick={handleClick} value="4">
+        4
+      </button>
+      <button onClick={handleClick} value="5">
+        5
+      </button>
+      <button onClick={handleClick} value="6">
+        6
+      </button>
+      <button onClick={handleClick} value="7">
+        7
+      </button>
+      <button onClick={handleClick} value="8">
+        8
+      </button>
+      <button onClick={handleClick} value="9">
+        9
+      </button>
+      <button onClick={handleClick} value=".">
+        .
+      </button>
+      <button onClick={handleDisabled} disabled={props.disabled} value="+">
+        +
+      </button>
+      <button onClick={handleDisabled} disabled={props.disabled} value="-">
+        -
+      </button>
+      <button onClick={handleDisabled} disabled={props.disabled} value="*">
+        *
+      </button>
+      <button onClick={handleDisabled} disabled={props.disabled} value="/">
+        /
+      </button>
+      <button onClick={handleDisabled} disabled={props.disabled} value="%">
+        %
+      </button>
+      <button onClick={props.handleResult} disabled={props.disabled}>
+        =
+      </button>
+      <button onClick={props.handleClear}>AC</button>
+    </div>
+  );
 }
 
-const Numbers = (props) => {
-    return(
-        <div>
-            <h1>{props.results}</h1>
-        </div>
-    )
-}
+const Numbers = props => {
+  return (
+    <div>
+      <h1>{props.results}</h1>
+    </div>
+  );
+};
 
-ReactDOM.render(<Calculator />, document.getElementById('app'))
+ReactDOM.render(<Calculator />, document.getElementById("app"));
